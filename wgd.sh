790705a55fab142d569cfb3d066739f130be0400
SamSandq <50418806+SamSandq@users.noreply.github.com>
2024-12-29 13:49:41 +0200

    Update wgd.sh

--- wgd.sh
+++ wgd.sh
@@ -1,5 +1,3 @@
 # compile GUI version with DEBUG
-# use new main.c for simplicity
 # execute in <root>, binary to ./bin and named wgd
-# cd /Volumes/SAMSUNG/wren-project
-clang -w -g -o bin/wgd -DDEBUG -DGUI -DS_SQL -framework Cocoa -framework AVFoundation -framework AVKit -framework Quartz -framework UniformTypeIdentifiers src/main.c src/wren-binding.c src/wren-gui.m -Iinclude lib/libwren_d.a #-l sqlite3
+clang -w -g -o bin/wgd -DDEBUG -framework Cocoa -framework AVFoundation -framework AVKit -framework Quartz -framework UniformTypeIdentifiers src/main.c src/wren-binding.c src/wren-gui.m -Iinclude lib/libwren_d.a
